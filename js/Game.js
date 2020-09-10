2d95f488cd9ff80242ad52e66ca514f2ae19383c
cameronconway3 <49353534+cameronconway3@users.noreply.github.com>
2020-09-10 15:53:38 +0100

    key press user interaction

--- js/Game.js
+++ js/Game.js
@@ -62,8 +62,12 @@ class Game {
     };
 
 
-    handleInteraction() {
-
+    /**
+    * Handles onscreen keyboard button clicks
+    * @param (HTMLButtonElement) button - The clicked button element
+    */
+    handleInteraction(button) {
+        console.log(button);
     }
 
     /**
--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 // console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
 
-// let game;
+let game;
 
 // // Assign the Start Game button to 'startGame' variable
 const startGame = document.querySelector("#btn__reset");
@@ -16,6 +16,29 @@ startGame.addEventListener('click', () => {
     game = new Game();
     game.startGame();
 });
+
+
+// Get all elements with keyrow class
+const keyrows = document.querySelectorAll(".keyrow");
+let keyElements = [];
+// Create a nexted loop to loop over the individual letter li elements in each keyrow element.
+// Push each individual li element into empty array - 'keyElements'
+keyrows.forEach( keyrow => {
+    for (let i = 0; i < keyrow.children.length; i++) {
+        keyElements.push(keyrow.children[i]);
+    }
+});
+
+// For each li element in 'keyElements' listen out for a click event
+keyElements.forEach( element => {
+    element.addEventListener("click", e => {
+        // console.log(e.target.textContent);
+        game.handleInteraction(e.target)
+    });
+
+});
+
+
 
 
 
@@ -30,8 +53,3 @@ startGame.addEventListener('click', () => {
 // Test removeLife
 // const game = new Game();
 // game.activePhrase.checkLetter('a');
-
-
-
-
-
