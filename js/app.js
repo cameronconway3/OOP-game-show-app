43eee870e28fa7af449c803481fc8bc30f56bb68
cameronconway3 <49353534+cameronconway3@users.noreply.github.com>
2020-09-10 19:08:46 +0100

    Extra credit functionality

--- js/app.js
+++ js/app.js
@@ -55,7 +55,103 @@ keyrows.forEach( keyrow => {
 // For each li element in 'keyElements' listen out for a click event
 keyElements.forEach( element => {
     element.addEventListener("click", e => {
-        // console.log(e.target.textContent);
         game.handleInteraction(e.target);
     });
+});
+
+
+/**
+ * Extra Credit
+ * 
+ * The handleInteraction() function takes a button element as an argument.
+ * The logic below creates an object of each element textContent (a-z) and pairs it with the relative button element.
+ * When the keydown event is triggered, each keyboard character (a-z) calls the handleInteraction() function.
+ * The relative element is passed as an arguement and accessed in 'keyElementPair' using dot notation. 
+ */
+let keyElementPair = {}
+keyElements.forEach( element => {
+    keyElementPair[element.textContent] = element;
+});
+
+document.addEventListener('keydown', e => {
+    switch(e.key) {
+        case "a":
+            game.handleInteraction(keyElementPair.a);
+            break;
+        case "b":
+            game.handleInteraction(keyElementPair.b);
+            break;
+        case "c":
+            game.handleInteraction(keyElementPair.c);
+            break;
+        case "d":
+            game.handleInteraction(keyElementPair.d);
+            break;
+        case "e":
+            game.handleInteraction(keyElementPair.e);
+            break;
+        case "f":
+            game.handleInteraction(keyElementPair.f);
+            break;
+        case "g":
+            game.handleInteraction(keyElementPair.g);
+            break;
+        case "h":
+            game.handleInteraction(keyElementPair.h);
+            break;
+        case "i":
+            game.handleInteraction(keyElementPair.i);
+            break;
+        case "j":
+            game.handleInteraction(keyElementPair.j);
+            break;
+        case "k":
+            game.handleInteraction(keyElementPair.k);
+            break;
+        case "l":
+            game.handleInteraction(keyElementPair.l);
+            break;
+        case "m":
+            game.handleInteraction(keyElementPair.m);
+            break;
+        case "n":
+            game.handleInteraction(keyElementPair.n);
+            break;
+        case "o":
+            game.handleInteraction(keyElementPair.o);
+            break;
+        case "p":
+            game.handleInteraction(keyElementPair.p);
+            break;
+        case "q":
+            game.handleInteraction(keyElementPair.q);
+            break;
+        case "r":
+            game.handleInteraction(keyElementPair.r);
+            break;
+        case "s":
+            game.handleInteraction(keyElementPair.s);
+            break;
+        case "t":
+            game.handleInteraction(keyElementPair.t);
+            break;
+        case "u":
+            game.handleInteraction(keyElementPair.u);
+            break;
+        case "v":
+            game.handleInteraction(keyElementPair.v);
+            break;
+        case "w":
+            game.handleInteraction(keyElementPair.w);
+            break;
+        case "x":
+            game.handleInteraction(keyElementPair.x);
+            break;
+        case "y":
+            game.handleInteraction(keyElementPair.y);
+            break;
+        case "z":
+            game.handleInteraction(keyElementPair.z);
+            break;
+    };
 });
